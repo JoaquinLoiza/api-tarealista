8a76f96aaf3a45f723e7140d3c6e25b8f0aee7f5
Joaquin Loiza <loiza.joaquin@gmail.com>
2023-11-29 19:14:36 -0300

    agrega modulo cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routesProjects = require('./routes/projects.routes.js');
 const errorsController = require('./controllers/errors.controller.js');
+const cors = require('cors');
 const config = require("./config.js");
 
 const port = config.PORT;
@@ -8,8 +9,22 @@ const hostname = config.HOST;
 
 const app = express();
 
+const originList = process.env.ORIGIN_LIST.split(',');
+
+// Configurar CORS con una lista de direcciones permitidas
+const corsOptions = {
+    origin: 'http://localhost:5500',
+    methods: 'GET,HEAD,POST,PUT,DELETE',
+    credentials: true, // Habilitar el intercambio de cookies en las solicitudes CORS
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+
 app.use(express.json());
-app.use('/api',routesProjects);
+app.use('/api', routesProjects);
 app.use('*', errorsController.resourceNotFound);
 app.use(errorsController.serverError);
 
